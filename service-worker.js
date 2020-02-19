348c649ffbe019ea367f681f2c926865915f7f73
Actireon <eheieiei37@gmail.com>
2020-02-19 23:52:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.16ed7d7460d155566c0e8aa1765dab8d.js"
 );
 
 self.addEventListener('message', (event) => {
